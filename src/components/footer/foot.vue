3912f1730252463e83baf18214940599c9080ce9
Zehuan Huang <zewanhuang@126.com>
2021-06-07 20:40:30 +0800

    feat: beautify footer

--- src/components/footer/foot.vue
+++ src/components/footer/foot.vue
@@ -1,15 +1,26 @@
 <template>
   <div id="foot" >
-    <el-menu class="el-menu-demo" mode="horizontal" >
-      <p class="copyright">Copyright 2021 zewan.top</p>
-      <router-link to="/team" class="team">团队</router-link>
-      <router-link to="/aboutus" class="aboutus">关于我们</router-link>
-      <router-link to="/">
-        <img class="logo" src="../../assets/logo_foot.png" alt="logo" height="40px" width="40px">
-      </router-link>
-      <p><a href="https://beian.miit.gov.cn/" target="_blank" class="beian">备案号：粤ICP备2021021248号-1</a></p>
-      <router-link to="/contactus" class="contactus">联系我们</router-link>
-    </el-menu>
+    <el-row>
+      <el-col :span=5 class="center font-wrap">
+        <router-link to="/">
+          <a class="font">Copyright 2021 zewan.top</a>
+        </router-link>
+      </el-col>
+      <el-col :span=5 class="center font-wrap">
+        <router-link to="/about-us" class="font">关于我们</router-link>
+      </el-col>
+      <el-col :span=4 class="center">
+        <router-link to="/" class="center">
+          <img class="logo" src="../../assets/logo_foot.png" alt="logo" height="40px" width="40px">
+        </router-link>
+      </el-col>
+      <el-col :span=5 class="center font-wrap">
+        <router-link to="/contactus" class="font">联系我们</router-link>
+      </el-col>
+      <el-col :span=5 class="center font-wrap">
+        <a href="https://beian.miit.gov.cn/" target="_blank" class="font">备案号：粤ICP备2021021248号-1</a>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -20,64 +31,28 @@ export default {
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap');
-  .copyright{
-    text-align: center;
-    float: left;
-    font-size: 12px;
-    padding: 10px;
-  }
-
-  .team{
-    position: absolute;
-    left: 12%;
-    text-align: center;
-    float: left;
-    font-size: 12px;
-    padding: 20px;
-    margin-left: 10%;
-    color: black;
-  }
-  .aboutus{
-    position: absolute;
-    left: 30%;
-    text-align: center;
-    float: left;
-    font-size: 12px;
-    padding: 20px;
-    margin-left: 4%;
-    color: black;
-  }
-  img.logo{
-    position: absolute;
-    left: 50%;
-    float: left;
-    margin-left: 0;
-    margin-top: 8px;
-  }
-  .beian{
-    position: absolute;
-    right: 0;
-    text-align: center;
-    float: right;
-    font-size: 12px;
-    padding-top: 5px;
-    margin-right: 4%;
-    color: black;
-  }
-  .contactus{
-    position: absolute;
-    right: 30%;
-    text-align: center;
-    float:right;
-    font-size: 12px;
-    padding-top: 5px;
-    margin-right: 4%;
-    color: black;
-  }
-
-  #foot{
-    margin-top: 50px;
-    bottom: 0;
-  }
+.center {
+  text-align: center;
+}
+.font {
+  color: gray;
+}
+#foot {
+  padding-left: 5%;
+  padding-right: 5%;
+  text-align: center;
+  font-size: 13px;
+  height: 70px;
+  box-shadow: 0 0 0 0 lightgray;
+}
+#foot .el-col {
+  margin-top: 10px;
+  height: 40px;
+}
+.logo {
+  margin-bottom: 25px;
+}
+.font-wrap {
+  padding-top: 20px;
+}
 </style>
